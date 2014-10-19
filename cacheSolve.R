c2a0c2e09c658a4a58f758086eaadb3d76df64f4
Robert Chan <Chan8903@gmail.com>
2014-10-19 16:44:14 -0400

    more changes

---
+++ cacheSolve.R
@@ -0,0 +1,25 @@
+## Computes the inverse. If the inverse has already been calculated (and the matrix has not changed),
+
+## then the cachesolve should retrieve the inverse from the cach
+
+cacheSolve <- function(x, ...) {
+  
+  i <- x$getinverse()
+  
+  if (!is.null(i)){
+    #check to see if the inverse is in the cache if yes, then get it
+    message("getting cached data")
+    
+    return(i)
+    
+  }
+  
+  data <- x$get() #inverse not in the cache, so calculate it
+  
+  i <- solve(data, ...)
+  
+  x$setinverse(i)
+  
+  i
+  
+}
